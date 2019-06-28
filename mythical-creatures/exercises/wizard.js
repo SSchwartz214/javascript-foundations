ef03d48f6eb783da4adb42ca284b602b37cbd8da
Seth Schwartz <sschwartz214@gmail.com>
2019-06-28 16:09:34 -0500

    Completes wizard

---
+++ mythical-creatures/exercises/wizard.js
@@ -0,0 +1,25 @@
+class Wizard {
+  constructor({name, bearded = true, isRested = true}) {
+    this.name = name
+    this.bearded = bearded
+    // this.bearded = data.bearded === undefined ? true : data.bearded;
+    this.isRested = isRested
+    this.castCount = 0
+  }
+
+  incantation(spell) {
+    return spell.toUpperCase();
+  }
+
+  cast() {
+    this.castCount += 1;
+    if (this.castCount <= 2)
+      return 'MAGIC BULLET'
+    else {
+      this.isRested = false
+      return 'I SHALL NOT CAST!'
+    }
+  }
+}
+
+module.exports = Wizard
